18c848a37600354903b659af1e9a558c59b452ec
kingingo <kingingohd@googlemail.com>
2014-07-26 15:45:49 +0200

    Rush

---
+++ kArcade/src/me/kingingo/karcade/Game/Games/SheepWars/SheepWars.java
@@ -0,0 +1,12 @@
+package me.kingingo.karcade.Game.Games.SheepWars;
+
+import me.kingingo.karcade.kArcadeManager;
+import me.kingingo.karcade.Game.Games.TeamGame;
+
+public class SheepWars extends TeamGame{
+
+	public SheepWars(kArcadeManager manager){
+		super(manager);	
+	}
+	
+}
